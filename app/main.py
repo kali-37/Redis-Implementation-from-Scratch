3cf91d4f990f70d58298a778c8d977d3ced7b27e
kall37 <rimalsubash77@gmail.com>
2025-04-27 17:31:26 +0545

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,7 +6,9 @@ def main():
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     con ,usr_addr = server_socket.accept() 
-    con.sendall(b"+PONG\r\n")
+    while True:
+        con.recv(1024)
+        con.send(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
